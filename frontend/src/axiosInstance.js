05ece336adc64d0d8d8efebf3ce3e06e5c967bcc
Shrutika Desai <desaishrutika21@gmail.com>
2026-02-20 17:46:33 +0530

    base url changed

--- frontend/src/axiosInstance.js
+++ frontend/src/axiosInstance.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://192.168.0.103:8000/api",
+  // baseURL: "http://192.168.0.103:8000/api",
 
-  // baseURL: "https://staging.abhinavcareerscope.com/api",
+  baseURL: "https://staging.abhinavcareerscope.com/api",
 
 
 });
