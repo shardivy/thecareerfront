40286c30ba486df3b83d0d434225cb98aa3be4d2
Shweta Bamane <shwetabbamane@gmail.com>
2026-04-23 17:48:20 +0530

    last name update

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -73,7 +73,7 @@ class User(AbstractBaseUser, PermissionsMixin):
         blank=True
     )
     first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100, null=True, blank=True)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=15, null=True, blank=True)
     # password = models.CharField(max_length=128, null=True, blank=True)
