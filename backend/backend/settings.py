24c37425f2ba1f577dbe87503b5b5f7a8ce9d7be
shardivy <divyanshsharma844@gmail.com>
2026-07-17 10:25:00 +0530

    Updated settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -437,6 +437,7 @@ AWS_S3_OBJECT_PARAMETERS = {
     "CacheControl": "max-age=86400",
 }
 
+AWS_S3_ENDPOINT_URL = f"https://s3.{AWS_S3_REGION_NAME}.amazonaws.com"
 AWS_S3_ADDRESSING_STYLE = "virtual"
 
 AWS_S3_VERIFY = True
