5701ea013020483d9fc59d4e2fb534d82cb55d97
Shrutika Desai <desaishrutika21@gmail.com>
2026-08-18 12:47:12 +0530

    added func to navigate on aptitude test management

--- frontend/src/axiosInstance.js
+++ frontend/src/axiosInstance.js
@@ -1,7 +1,7 @@
  import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: "http://192.168.1.9:8000/api/",
+  // baseURL: "http://192.168.141.38:8001/api/",
   // baseURL: "http://192.168.247.38:8002/api",
   // baseURL: "http://10.253.15.38:8002/api",
   // baseURL: "https://fares-seek-adam-interracial.trycloudflare.com/api",
